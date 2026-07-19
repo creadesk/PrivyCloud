0b276df8678dd885af0ac0757d2a063f00c9af0a
creadesk <wallroofbasement@proton.me>
2026-07-19 18:53:03 +0200

    Initial commit – Projekt‑Setup

--- config/admin.py
+++ config/admin.py
@@ -7,3 +7,14 @@ class PlatformSettingAdmin(admin.ModelAdmin):
   list_display = ("key", "value", "description")
   search_fields = ("key", "value")
 '''
+
+from django.contrib import admin
+from django.apps import apps
+
+app_config = apps.get_app_config("django_smart_ratelimit")
+
+for model in app_config.get_models():
+    try:
+        admin.site.unregister(model)
+    except admin.sites.NotRegistered:
+        pass
